59f8f12f4d4d0e16820cf3ee599af3703cdee21f
kasanipavani2 <kasani.pavani2@gmail.com>
2025-12-11 18:21:41 +0530

    Added file

---
+++ module-3/Modular Javascript/components/footer.js
@@ -0,0 +1,7 @@
+export function Footer() {
+  return `
+    <footer style="padding: 10px; background:#eee; margin-top:20px; text-align:center;">
+      <p>© 2025 Modular App | All Rights Reserved</p>
+    </footer>
+  `;
+}
